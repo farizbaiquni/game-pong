195f45a511252d8968b836c5e11a1cc806589a54
Fariz Baiquni <farizbaiquni@gmail.com>
2020-03-28 00:44:08 +0700

    Commit

---
+++ Assets/ballscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballscript : MonoBehaviour
+{
+
+	public int speed = 30;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World!");
+        GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other){
+    	if(other.collider.name == "WallVertical-Kiri" || other.collider.name == "WallVertical-Kanan"){
+    		GetComponent<Transform>().position = new Vector2(0,0);
+    	}
+    }
+}
